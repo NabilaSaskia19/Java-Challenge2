32b09bf15ee1d3ec19aee2bd198b4c7c69850e1d
NabilaSaskia19 <91514373+NabilaSaskia19@users.noreply.github.com>
2021-10-14 14:03:29 +0700

    challenge 2

---
+++ JavaBasic2.java
@@ -0,0 +1,14 @@
+// tentang if else
+
+public class JavaBasic2 {
+
+    public static void main(String[] args){
+        String value = "A";
+
+        if(value == "B"){
+            System.out.println(false);
+        } else {
+            System.out.println(true);
+        }
+    }
+}
